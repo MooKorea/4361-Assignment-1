b5297cd2cdddf4d9926cf05b9f83a6948e3fd113
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-09-24 13:12:09 -0500

    intro video fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,35 @@
-import '../styles/style.scss'
+import "../styles/style.scss";
+import anime from "animejs";
 
-import { muteButton } from './muteButton.js'
-muteButton()
+import { muteButton } from "./muteButton.js";
+muteButton();
 
-const introVideo = document.querySelector(".intro-video")
+const introVideo = document.querySelector(".intro-video");
+const video = introVideo.querySelector("video");
+video.load();
+video.onloadeddata = () => {
+  anime({
+    targets: ".intro-video-src",
+    opacity: 1,
+    complete: () => {
+      introVideo.style.backgroundColor = "";
+    },
+  });
+};
 
 const handleEnterSite = () => {
-    introVideo.querySelector("video").play()
-    document.querySelector(".background-music").play()
-    introVideo.style.pointerEvents = "none"
-}
+  video.play();
+  document.querySelector(".background-music").play();
+  introVideo.style.pointerEvents = "none";
 
-introVideo.addEventListener("click", handleEnterSite)
+  //fade video after 2 seconds
+  setTimeout(() => {
+    anime({
+        targets: ".intro-video-src",
+        opacity: 0,
+      });
+    document.querySelector("body").style.overflowY = "auto"
+  }, 2000);
+};
+
+introVideo.addEventListener("click", handleEnterSite);
