8adc0cc6c3fe845075769a626a673dcde0a13e24
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-09-24 01:14:58 -0500

    click to enter site

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,13 @@ import '../styles/style.scss'
 
 import { muteButton } from './muteButton.js'
 muteButton()
+
+const introVideo = document.querySelector(".intro-video")
+
+const handleEnterSite = () => {
+    introVideo.querySelector("video").play()
+    document.querySelector(".background-music").play()
+    introVideo.style.pointerEvents = "none"
+}
+
+introVideo.addEventListener("click", handleEnterSite)
--- scripts/muteButton.js
+++ scripts/muteButton.js
@@ -2,7 +2,7 @@ export function muteButton() {
   const muteButton = document.querySelector(".mute-button");
   const backGroundMusic = document.querySelector(".background-music");
   backGroundMusic.volume = 0.5;
-
+  
   let isMuted = false;
   const handleMuteButton = () => {
     isMuted = !isMuted;
