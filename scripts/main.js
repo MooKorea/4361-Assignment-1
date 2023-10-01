dda0d30119e1db91c383563d5520673280c11269
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-10-01 14:01:32 -0500

    add body3

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,5 @@ import "../styles/style.scss";
 import { body1 } from "./body1.js"
 body1();
 
-// import { muteButton } from "./muteButton.js";
-// muteButton();
-
 import { enterSite } from "./enterSite";
 enterSite();
