a780b3417b210b6b7de8a954973552c402c14df9
MooKorea <69235304+MooKorea@users.noreply.github.com>
2023-10-09 23:31:41 -0500

    switch firebase project

--- scripts/firebaseConfig.js
+++ scripts/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKsTDUYMtApskYd-cZWgzd0vIz71pXUTA",
-  authDomain: "eat-at-baratie.firebaseapp.com",
-  projectId: "eat-at-baratie",
-  storageBucket: "eat-at-baratie.appspot.com",
-  messagingSenderId: "369065987076",
-  appId: "1:369065987076:web:27edf5bf330c98fd2f3951",
-  measurementId: "G-78K81L3DME",
+  apiKey: "AIzaSyAna_PDDu-Pe4Ciyo4zWdIuwjI8Q4-gONc",
+  authDomain: "visitbaratie.firebaseapp.com",
+  projectId: "visitbaratie",
+  storageBucket: "visitbaratie.appspot.com",
+  messagingSenderId: "395386304098",
+  appId: "1:395386304098:web:f70ffdfa9ffb58df0f033b",
+  measurementId: "G-BCHZDSX71X"
 };
 
 const config = {};
